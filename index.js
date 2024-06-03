5854972b06cd99fff6b2ef900aaa6711f6b701d2
Le Hoang Nam <lehoangnam.31123@gmail.com>
2024-06-03 23:52:37 +0700

    Modify: Change content log <port 3000> - <port 3030> and add ! in res.send()

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.listen(3030, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3030');
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!');
 });
