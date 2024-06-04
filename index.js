2fc6680168f07ba1869ebe881c38dc049864464f
Le Hoang Nam <lehoangnam.31123@gmail.com>
2024-06-04 11:11:29 +0700

    Add:  create folder static public add UI in this. Define folder public static in index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.listen(3030, () => {
-    console.log('Server is running on port 3030');
+app.use(express.static('public')); // Sử dụng file tĩnh trong thư mục public
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Xin chào tôi là Lê Hoàng Nam người đã tạo ra trang web này');
 });
