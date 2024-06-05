c373e2e8e59ba24c2a55f1a3a61a12746291a99d
Le Hoang Nam <lehoangnam.31123@gmail.com>
2024-06-05 10:50:31 +0700

    Update: import view engine ejs, set view to use ejs set('view engine', 'ejs'), create routes with syntax ejs + express

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejs = require('ejs');
 
 app.use(express.static('public')); // Sử dụng file tĩnh trong thư mục public
 
@@ -8,6 +9,26 @@ app.listen(4000, () => {
     console.log('Server is running on port 4000');
 });
 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './pages/index.html'));
+// });
+
+// Sử dụng EJS
+app.set('view engine', 'ejs');
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './pages/index.html'));
+    res.render('index');
 });
+
+app.get('.about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+
+app.get('/post', (req, res) => {
+    res.render('post');
+})
