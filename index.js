ab57a726422878c2dd45d575b60579ce2b85dd80
le hoang nam <lehoangnam.31123@gmail.com>
2024-06-23 22:33:51 +0700

    Modify: change url add / to become absolute url. Change content form in container use two fields title and content with form use method POST and action /posts/store. Install: Install body-parser to see content easy form JSON (npm install body-parser)

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const app = new express();
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Kết nối MongoDB
-mongoose.connect('mongodb://localhost:27017/clean_blog', { useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/clean_blog');
 
 app.use(express.static('public')); // Sử dụng file tĩnh trong thư mục public
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, './pages/index.html'));
@@ -39,4 +43,9 @@ app.get('/post', (req, res) => {
 
 app.get('/posts/new', (req, res) => {
     res.render('create');
+});
+
+app.post('/posts/store', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
 });
